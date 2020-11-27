4f0d04d9724e79e19f3916b325c7d8dc8416bbfc
Ernest Wong <771ernestwong@gmail.com>
2020-11-27 15:11:14 -0500

    suh

--- src/ErnestScratch.cpp
+++ src/ErnestScratch.cpp
@@ -1,15 +1,28 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include <MCP23017.h>
+#include <SPI.h>
+#include <Wire.h>
+
+
+#define MCP23017_ADDR 0x20
 
 int status = WL_IDLE_STATUS;
 
 char ssid[] = ""; //  your network SSID (name)
 char pass[] = ""; // your network password
 char adafruitUsername[] = "mendozamartin";
-char adafruitKey[] = "";
+char adafruitKey[] = "aio_Siho87ybOa5o0GQX2Pd8Uhesr3m4";
 char server[] = "io.adafruit.com";
 int port = 1883;
+
+// Define an instance of the MCP23017 class
+MCP23017 mcp = MCP23017(MCP23017_ADDR);
+
+void portMode(MCP23017Port port, uint8_t directions, uint8_t pullups = 0xFF, uint8_t inverted = 0x00);
+void writeRegister(MCP23017Register reg, uint8_t value);
+
 
 void callback(char *topic, byte *payload, unsigned int length);
 void connectToMQTT();
@@ -21,7 +34,19 @@ PubSubClient mqttClient(server, port, callback, client);
 void setup()
 {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+
+  // Join I2C bus - more on this in later chapter 8)
+  Wire.begin();
+  // Initialize MCP
+  mcp.init();
+  // Port A as Output (LEDS) || Port B as Input (Switches & Buttons)
+  mcp.portMode(MCP23017Port::A, 0);                             // 0 = Pin is configured as an output.
+  mcp.portMode(MCP23017Port::B, 0b11111111);              // 1 = Pin is configured as an input.
+  // Reset Port A & B
+  mcp.writeRegister(MCP23017Register::GPIO_A, 0x00);  //Reset port A 
+  mcp.writeRegister(MCP23017Register::GPIO_B, 0x00);  //Reset port B
+
+  Serial.begin(9600); /*
   // Connect to Wi-Fi network with SSID and password
   Serial.print("Connecting to ");
   Serial.println(ssid);
@@ -36,18 +61,43 @@ void setup()
   Serial.println("WiFi connected.");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+*/
+  mcp.pinMode(12, INPUT_PULLUP, true);    // sets the digital pin input
 }
 
-void loop()
+int soapCount = 10; // variables for my soap thing goes hereeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+int butVal = 0;
+int butValPrev = 0;
+int soapUsers = 0;
+
+void loop() // mainloop here
 {
+  butVal = mcp.digitalRead(12);
+
+  if ((butVal == 1) && (butValPrev == 0)) {
+    if (soapCount >0){
+      soapCount--; 
+      soapUsers++;
+      Serial.print("soap level is currently: "); 
+      Serial.println(soapCount);
+    }
+    else {
+      Serial.print("soap level is currently: ");
+      Serial.print(soapCount); 
+      Serial.println("   dat shii eMPTYY");
+    }
+  }
+  
+  butValPrev = butVal;
+
+  /*
   // put your main code here, to run repeatedly:
   if (!mqttClient.connected())
   {
     reconnect();
   }
-
-  mqttClient.loop();
-  delay(1000);
+  mqttClient.loop(); */
+  //delay(1000); 
 }
 
 void callback(char *topic, byte *payload, unsigned int length)
